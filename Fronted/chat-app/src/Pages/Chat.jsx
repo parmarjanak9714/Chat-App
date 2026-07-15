80c5406b0e8dd99d53ea5be9ae980bac4b071ae0
parmar janak <parmarjanak9714@gmail.com>
2026-07-15 15:23:09 +0530

    chat.jsx update

--- Fronted/chat-app/src/Pages/Chat.jsx
+++ Fronted/chat-app/src/Pages/Chat.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ChatBox from '../Componets/ChatBox'
 import SideBar from '../Componets/SideBar'
 import { useState } from 'react'
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
